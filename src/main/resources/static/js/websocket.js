a364b5a754349433ecfccc3e75766a175b3ec880
hwan20c <hwan20c@gmail.com>
2022-08-29 18:52:21 +0900

    amazon server ip insert

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 	//const websocket = new WebSocket("ws://localhost:8080/ws/chat");
+	const websocket = new WebSocket("ws://3.39.231.146:8080/ws/chat");
 	
 	websocket.onmessage = onMessage;
 	websocket.onopen = ipCheck;

6c72e22c4c84f9b72531d3a48224e85b174aa331
hwan20c <hwan20c@gmail.com>
2022-09-15 00:19:26 +0900

    weather http 관련 test

--- src/main/resources/static/js/main/weather.js
+++ src/main/resources/static/js/main/weather.js
@@ -1,9 +1,10 @@
 const API_KEY = "a729f855a8412f6352e55d27a5978153";
 
 function onGeoOk(position) {
+    console.log(position);
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -13,8 +14,8 @@ function onGeoOk(position) {
             weather.innerText = `${data.weather[0].main} \ ${data.main.temp}`;
         });
 }
-function onGeoError() {
-    alert("Can't find you. No weather for you.");
-}
+// function onGeoError() {
+//     alert("Can't find you. No weather for you.");
+// }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk);

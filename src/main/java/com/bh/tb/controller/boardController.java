a6d286ad370896b667655aee2a6601be10990c42
hwan20c <hwan20c@gmail.com>
2022-09-21 02:55:11 +0900

    myboard init

---
+++ src/main/java/com/bh/tb/controller/boardController.java
@@ -0,0 +1,16 @@
+package com.bh.tb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import groovyjarjarpicocli.CommandLine.Model;
+
+@Controller
+public class boardController {
+
+    @GetMapping("/myBoard")
+	public String mainpage(Model model) {
+		return "myBoard";
+	}
+    
+}

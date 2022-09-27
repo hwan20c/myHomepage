24cff5f507cb508db36d6bad2dbbe0e04724d4e3
hwan20c <hwan20c@gmail.com>
2022-09-27 22:44:40 +0900

    myboard modify

--- src/main/java/com/bh/tb/controller/boardController.java
+++ src/main/java/com/bh/tb/controller/boardController.java
@@ -2,7 +2,6 @@ package com.bh.tb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import groovyjarjarpicocli.CommandLine.Model;
 
 @Controller

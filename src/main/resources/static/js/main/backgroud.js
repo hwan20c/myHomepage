8f0b48d9fb09072085674697a7a9d1d33185b911
hwan20c <hwan20c@gmail.com>
2022-09-30 16:11:35 +0900

    index page modify / footer modify

--- src/main/resources/static/js/main/backgroud.js
+++ src/main/resources/static/js/main/backgroud.js
@@ -7,6 +7,8 @@ const images = [
 ]
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
+const footer = document.querySelector("#base_footer");
+const todoBtn = document.getElementById("openBtn");
 
 bgImage.src = `./img/main/${chosenImage}`;
 document.body.style.backgroundImage = `url(${bgImage.src})`;
@@ -14,3 +16,7 @@ document.body.style.height = "100%";
 document.body.style.backgroundPosition = "center";
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
+
+footer.className += " fixed-bottom";
+footer.style.zIndex = "1";
+todoBtn.style.zIndex = "2";

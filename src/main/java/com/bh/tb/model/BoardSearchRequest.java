ba5da985499260f579bec6da0f65180c5fb58168
hwan20c <hwan20c@gmail.com>
2022-10-10 21:51:07 +0900

    board api modify

--- src/main/java/com/bh/tb/model/BoardSearchRequest.java
+++ src/main/java/com/bh/tb/model/BoardSearchRequest.java
@@ -1,13 +1,16 @@
 package com.bh.tb.model;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class BoardSearchRequest {
   private int page = 0;
-  private int size = 10;
+  private int size = 9;
   private int type = 0;
   private String searchKey;
   private String searchValue;

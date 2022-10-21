7417ce83ea3d685f6fe74d7705ace283f458a12d
hwan20c <hwan20c@gmail.com>
2022-10-21 17:30:51 +0900

    jasypt change + gitignore update

--- src/main/java/com/bh/tb/TbApplication.java
+++ src/main/java/com/bh/tb/TbApplication.java
@@ -11,31 +11,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 @EnableEncryptableProperties
 public class TbApplication {
 
-	// @Value("${jasypt.encryptor.password}")
-	// private final static String adsf = "";
-
 	public static void main(String[] args) {
-		// PooledPBEStringEncryptor standardPBEStringEncryptor = new PooledPBEStringEncryptor ();
-		// SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		// config.setPassword("HWAN@1218");
-    // config.setAlgorithm("PBEWithMD5AndDES");
-    // config.setKeyObtentionIterations("1000");
-    // config.setPoolSize("1");
-    // config.setProviderName("SunJCE");
-    // config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-    // config.setStringOutputType("base64");
-		// standardPBEStringEncryptor.setConfig(config);
-
-		// String plainText = "AP_NORTHEAST_2";
-
-		// String encrpytedText = standardPBEStringEncryptor.encrypt(plainText);
-		// String decrpytedText = standardPBEStringEncryptor.decrypt(encrpytedText);
-
-		// System.out.println("@@@@@@@@@@@@@@@ " + encrpytedText);
-		// System.out.println("@@@@@@@@@@@@@@@ " + decrpytedText);
-
-		// System.out.println("@@@@@ " + adsf);
-		
 		SpringApplication.run(TbApplication.class, args);
 	}
 
--- src/main/java/com/bh/tb/config/JasyptConfig.java
+++ src/main/java/com/bh/tb/config/JasyptConfig.java
@@ -1,24 +1,25 @@
 package com.bh.tb.config;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class JasyptConfig {
-
-  // @Value("${jasypt.encryptor.password}")
-  // private static final String ENCRYPT_KEY = "";
-
-  private static final String ENCRYPT_KEY = "HWAN@1218";
 
   @Bean("jasyptStringEncryptor")
   public StringEncryptor stringEncryptor() {
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-    config.setPassword(ENCRYPT_KEY);
+    config.setPassword(getJasyptPassword());
     config.setAlgorithm("PBEWithMD5AndDES");
     config.setKeyObtentionIterations("1000");
     config.setPoolSize("1");
@@ -27,5 +28,41 @@ public class JasyptConfig {
     config.setStringOutputType("base64");
     encryptor.setConfig(config);
     return encryptor;
+  }
+
+  private String getJasyptPassword() {
+    try {
+      ClassPathResource resource = new ClassPathResource("jasypt_password.txt");
+      return Files.readAllLines(Paths.get(resource.getURI())).stream()
+              .collect(Collectors.joining(" "));
+    } catch (IOException e) {
+      throw new RuntimeException("Not found Jasypt password");
+    }
+  }
+
+  public String getEncryptedPlainText(String plainText) {
+    PooledPBEStringEncryptor standardPBEStringEncryptor = new PooledPBEStringEncryptor ();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+
+    //SET YOUR PASSWORD
+		config.setPassword("");
+
+
+    config.setAlgorithm("PBEWithMD5AndDES");
+    config.setKeyObtentionIterations("1000");
+    config.setPoolSize("1");
+    config.setProviderName("SunJCE");
+    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+    config.setStringOutputType("base64");
+		standardPBEStringEncryptor.setConfig(config);
+
+		String encrpytedText = standardPBEStringEncryptor.encrypt(plainText);
+		String decrpytedText = standardPBEStringEncryptor.decrypt(encrpytedText);
+
+		System.out.println("@@@@@@@@@@@@@@@ " + encrpytedText);
+		System.out.println("@@@@@@@@@@@@@@@ " + decrpytedText);
+
+    return encrpytedText;
   }
 }

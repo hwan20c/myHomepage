3e09877a794eddc47c28ddadcab33046073a92a4
hwan20c <hwan20c@gmail.com>
2022-10-21 17:46:39 +0900

    main import delete

--- src/main/java/com/bh/tb/TbApplication.java
+++ src/main/java/com/bh/tb/TbApplication.java
@@ -1,7 +1,5 @@
 package com.bh.tb;
 
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

c30fe0b08805bb57f1d13677aeea841ce62a8d04
hwan20c <hwan20c@gmail.com>
2022-11-08 13:38:48 +0900

    git ignore update

--- src/main/java/com/bh/tb/TbApplication.java
+++ src/main/java/com/bh/tb/TbApplication.java
@@ -3,7 +3,6 @@ package com.bh.tb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.bh.tb.config.JasyptConfig;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
@@ -12,10 +11,6 @@ public class TbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TbApplication.class, args);
-
-		// JasyptConfig jasyptConfig = new JasyptConfig();
-
-		// jasyptConfig.getEncryptedPlainText("hwan255777!@");
 	}
 
 }
